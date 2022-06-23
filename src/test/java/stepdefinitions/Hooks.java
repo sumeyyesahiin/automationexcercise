d69f2ec608b53e7c06de272dba98399c3ce79633
sumeyye <sumshn7@gmail.com>
2022-06-23 14:29:45 +0300

    api

--- src/test/java/stepdefinitions/Hooks.java
+++ src/test/java/stepdefinitions/Hooks.java
@@ -1,10 +1,14 @@
 package stepdefinitions;
 
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import utilities.ConfigReader;
 import utilities.Driver;
 
 import java.io.File;
@@ -13,6 +17,13 @@ import java.io.IOException;
 import static utilities.Driver.waitFor;
 
 public class Hooks {
+
+    public static RequestSpecification spec;
+    @Before(value = "@ApiTestItem")
+    public void setup(){
+        spec = new RequestSpecBuilder().setBaseUri(ConfigReader.getProperty("url")).build();
+    }
+
 
     @After
     public void tearDown(Scenario scenario) {
